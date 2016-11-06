5588245e3327fd7715d51f0219f98c44222347eb
Kingwell <jinhua.leng@gmail.com>
2016-11-06 19:52:55 +0800

    add app

---
+++ pages/my/my.js
@@ -0,0 +1,24 @@
+var app = getApp();
+Page({
+    data:{
+        userInfo:{},
+        userName:'kingwell',
+        signature:'心若没有归属，走到发里都是流浪。'
+    },
+    onLoad:function(){
+        wx.setNavigationBarTitle({
+        title: '我'
+        });
+        var that = this
+        //调用应用实例的方法获取全局数据
+        app.getUserInfo(function(userInfo){
+            console.log(userInfo);
+        //更新数据
+        that.setData({
+            userInfo:userInfo
+        });
+        });
+
+        
+    }
+});
